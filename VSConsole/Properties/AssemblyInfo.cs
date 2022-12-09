80ce07b96928921226ce0c54c022d04a19a59a7b
Matt Lacey <matt@mrlacey.co.uk>
2022-12-09 21:37:42 +0000

    v1.1.1 - arm64 support
    + version release builds
    + use vsix manifest generator

--- VSConsole/Properties/AssemblyInfo.cs
+++ VSConsole/Properties/AssemblyInfo.cs
@@ -5,29 +5,16 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSConsole")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle(VSConsole.Vsix.Name)]
+[assembly: AssemblyDescription(VSConsole.Vsix.Description)]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSConsole")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany(VSConsole.Vsix.Author)]
+[assembly: AssemblyProduct(VSConsole.Vsix.Name)]
+[assembly: AssemblyCopyright(VSConsole.Vsix.Author)]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture(VSConsole.Vsix.Language)]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(VSConsole.Vsix.Version)]
+[assembly: AssemblyFileVersion(VSConsole.Vsix.Version)]
